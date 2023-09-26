a32902351847550482aec3d72e85571ff8d2468d
Put-in-228 <anonymousrustim@gmail.com>
2023-09-26 21:38:28 +0300

    removed old project, edited links for new reports from estonian

--- frontend/hanza-web-app/hanza-web-app/src/views/Dashboard.vue
+++ frontend/hanza-web-app/hanza-web-app/src/views/Dashboard.vue
@@ -32,22 +32,22 @@ export default {
         {
           title: 'Plasma 5110',
           embedUrl:
-            'https://app.powerbi.com/reportEmbed?reportId=d202cc2b-4847-47a2-ab95-b6cb433fdd84&autoAuth=true&ctid=3efd4d88-9b88-4fc9-b6c0-c7ca50f1db57',
+            'https://app.powerbi.com/reportEmbed?reportId=85dae09c-efce-41e2-923c-288c79a81416&autoAuth=true&ctid=3efd4d88-9b88-4fc9-b6c0-c7ca50f1db57',
         },
         {
           title: 'Laser 5210',
           embedUrl:
-            'https://app.powerbi.com/reportEmbed?reportId=7a8265f4-2cf1-4a44-8973-da37532c6747&autoAuth=true&ctid=3efd4d88-9b88-4fc9-b6c0-c7ca50f1db57',
+            'https://app.powerbi.com/reportEmbed?reportId=18f5fbba-cdfa-4b2c-8133-f95bd432deec&autoAuth=true&ctid=3efd4d88-9b88-4fc9-b6c0-c7ca50f1db57',
         },
         {
           title: 'Bending 5250',
           embedUrl:
-            'https://app.powerbi.com/reportEmbed?reportId=bc3ffa1b-c710-4b6b-a514-e0233dc9e356&autoAuth=true&ctid=3efd4d88-9b88-4fc9-b6c0-c7ca50f1db57',
+            'https://app.powerbi.com/reportEmbed?reportId=7b09dc23-52c3-44f7-9435-df65fd069c51&autoAuth=true&ctid=3efd4d88-9b88-4fc9-b6c0-c7ca50f1db57',
         },
         {
           title: 'Machining 5300',
           embedUrl:
-            'https://app.powerbi.com/reportEmbed?reportId=409db702-5ae4-4320-9fde-717c07e368d0&autoAuth=true&ctid=3efd4d88-9b88-4fc9-b6c0-c7ca50f1db57',
+            'https://app.powerbi.com/reportEmbed?reportId=6b9d077e-0ac7-481c-99f6-6cd30e5f4a7e&autoAuth=true&ctid=3efd4d88-9b88-4fc9-b6c0-c7ca50f1db57',
         },
       ],
     };
