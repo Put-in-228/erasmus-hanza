6764cfe4dd757cc6f2715bea98c2120e7b7c7ef3
Put-in-228 <anonymousrustim@gmail.com>
2023-09-26 11:16:26 +0300

    added report id's

--- frontend/hanza-web-app/hanza-web-app/src/components/ReportDashboard.vue
+++ frontend/hanza-web-app/hanza-web-app/src/components/ReportDashboard.vue
@@ -1,10 +1,31 @@
 <template>
-    <!-- <PowerBIEmbedModule
-    :embedConfig = {{
+    <PowerBIEmbedModule
+    :embedConfig = config,
+
+    :cssClassName = cssName
+
+    :phasedEmbedding = phasedEmbed
+    ></PowerBIEmbedModule>
+
+    <iframe title="new_proj_20_09" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=efb8d325-f98b-41a1-acc4-85d43b65c43a&autoAuth=true&ctid=3efd4d88-9b88-4fc9-b6c0-c7ca50f1db57" frameborder="0" allowFullScreen="true"></iframe>
+
+</template>
+
+<script>
+    import { PowerBIEmbedModule } from 'powerbi-client-vue-js';
+    export default {
+        components: {
+            PowerBIEmbedModule
+        }
+    
+    
+    }
+
+    var config = {
         type: "report",
-        id: "<Report Id>",
-        embedUrl: "<Embed Url>",
-        accessToken: "<Access Token>",
+        id: "efb8d325-f98b-41a1-acc4-85d43b65c43a",
+        embedUrl: undefined,
+        accessToken: "3efd4d88-9b88-4fc9-b6c0-c7ca50f1db57",
         tokenType: models.TokenType.Embed,
         settings: {
             panes: {
@@ -15,33 +36,11 @@
             },
             background: models.BackgroundType.Transparent,
         }
-    }}
 
-    :cssClassName = { "reportClass" }
+    };
 
-    :phasedEmbedding = { false }
+    var cssName = "reportCSS";
 
-    :eventHandlers = {
-        new Map([
-            ['loaded', () => console.log('Report loaded');],
-            ['rendered', () => console.log('Report rendered');],
-            ['error', (event) => console.log(event.detail);]
-        ])
-    }
-    ></PowerBIEmbedModule> -->
-
-    <iframe title="new_proj_20_09" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=efb8d325-f98b-41a1-acc4-85d43b65c43a&autoAuth=true&ctid=3efd4d88-9b88-4fc9-b6c0-c7ca50f1db57" frameborder="0" allowFullScreen="true"></iframe>
-
-</template>
-
-<script>
-    // import { PowerBIEmbedModule } from 'powerbi-client-vue-js';
-    // export default {
-    //     components: {
-    //         PowerBIEmbedModule
-    //     }
-    // 
-    //
-    // }
+    var phasedEmbed = false;
 </script>
 
--- frontend/hanza-web-app/hanza-web-app/src/views/Dashboard.vue
+++ frontend/hanza-web-app/hanza-web-app/src/views/Dashboard.vue
@@ -1,13 +1,16 @@
 <template>
     <div>
         <h1>Dashboard ID {{ dashboardId }}</h1>
-        <iframe title="new_proj_20_09" width="1140" height="541.25"
+        <!-- <iframe title="new_proj_20_09" width="1140" height="541.25"
             src="https://app.powerbi.com/reportEmbed?reportId=efb8d325-f98b-41a1-acc4-85d43b65c43a&autoAuth=true&ctid=3efd4d88-9b88-4fc9-b6c0-c7ca50f1db57"
-            frameborder="0" allowFullScreen="true"></iframe>
+            frameborder="0" allowFullScreen="true"></iframe> -->
+        <ReportDashboard />
     </div>
 </template>
   
 <script>
+import { ReportDashboard } from '../components/ReportDashboard.vue';
+
 export default {
     props: {
         dashboardId: {
@@ -40,3 +43,8 @@ h1 {
     padding: 10px;
 }
 </style>
+
+<!-- BENDING <iframe title="bending-pre-welding-3" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=bc3ffa1b-c710-4b6b-a514-e0233dc9e356&autoAuth=true&ctid=3efd4d88-9b88-4fc9-b6c0-c7ca50f1db57" frameborder="0" allowFullScreen="true"></iframe> -->
+<!-- LASER <iframe title="laser-pre-welding-2" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=7a8265f4-2cf1-4a44-8973-da37532c6747&autoAuth=true&ctid=3efd4d88-9b88-4fc9-b6c0-c7ca50f1db57" frameborder="0" allowFullScreen="true"></iframe> -->
+<!-- MACHINING <iframe title="machining-pre-welding-4" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=409db702-5ae4-4320-9fde-717c07e368d0&autoAuth=true&ctid=3efd4d88-9b88-4fc9-b6c0-c7ca50f1db57" frameborder="0" allowFullScreen="true"></iframe> -->
+<!-- PLASMA <iframe title="plasma-pre-welding-1" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=d202cc2b-4847-47a2-ab95-b6cb433fdd84&autoAuth=true&ctid=3efd4d88-9b88-4fc9-b6c0-c7ca50f1db57" frameborder="0" allowFullScreen="true"></iframe> -->
